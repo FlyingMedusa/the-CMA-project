ab917a46ff298dea45710f0c4e89dfa76cf73a93
Marta Sleboda <sleboda.m98@gmail.com>
2021-02-26 09:40:12 +0100

    files update

--- core.py
+++ core.py
@@ -1,53 +1,35 @@
-from flask import Flask, render_template, request, redirect
-# from flask_wtf import FlaskForm
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 import os
 from os.path import join, dirname, realpath
 import tables as tbl
 import graphs as gr
-import tempfile
 
 app = Flask(__name__)
 
-# app.config.update(
-#     SECRET_KEY='mykey',
-#     SQLALCHEMY_DATABASE_URI='postgresql://postgres:my password@localhost/catalog_db',
-#     SQLALCHEMY_TRACK_MODIFICATIONS=False
-# )
-
-# db = SQLAlchemy(app)
-
-# UPLOAD_FOLDER = 'static/files' -> when locally - it worked but that's not what I want
-#app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
-
-UPLOAD_FOLDER = tempfile.TemporaryDirectory()
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['SECRET_KEY'] = 'mykey'
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
 
-
 @app.route('/uploader', methods=['GET', 'POST'])
+
 def uploader():
     file = request.files['inputFile']
     if file.filename != '' and '.xls' in file.filename:
-        # newFile = FileContents(name=file.filename, data=file.read())
-        # db.session.add(newFile)
-        # db.session.commit()
-        # temporary!
-        # return 'Saved ' + file.filename + ' to the database!'
-        filename = 'file.xls'
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'].name, filename)
+        print(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
-
-        all_requirements = tbl.analysis(file_path)
+        
+        all_requirements = tbl.analysis()
         ts_top = all_requirements[0]
         abst_top = all_requirements[1]
         target = all_requirements[2]
@@ -60,11 +42,9 @@ def uploader():
         return render_template("data.html", ts_top=ts_top, abst_top=abst_top, source=source, target=target, graph1=graph1, graph2=graph2, graph3=graph3)
     return render_template('upload.html')
 
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
